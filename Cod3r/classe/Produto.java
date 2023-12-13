84e927f49c89332b1b83100b93dc59c78199a694
Gabriel <GabrielFelipeS@users.noreply.github.com>
2023-12-12 23:22:07 -0300

    Cod3r: Membro de Classe vs Membro de instansia

--- Cod3r/classe/Produto.java
+++ Cod3r/classe/Produto.java
@@ -3,7 +3,7 @@ package Cod3r.classe;
 public class Produto {
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto;
 	
 	double precoComDesconto() {
 		return this.preco * (1 - this.desconto);
--- Cod3r/classe/ProdutoTeste.java
+++ Cod3r/classe/ProdutoTeste.java
@@ -4,7 +4,7 @@ public class ProdutoTeste {
 	public static void main(String[] args) {
 		Produto produto = new Produto();
 		produto.nome = "Abacate";
-		produto.desconto = 0.1;
+		Produto.desconto = 0.1;
 		produto.preco = 200;
 		System.out.printf("O nome do produto é %s ele possui o preço %f e com desconto de %f fica %f", produto.nome,  produto.preco,  produto.desconto, produto.precoComDesconto());
 	}
