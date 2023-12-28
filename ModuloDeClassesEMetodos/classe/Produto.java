7f9b298db6cd0388f5af1757f9cf062057a2630d
Gabriel <GabrielFelipeS@users.noreply.github.com>
2023-12-28 18:15:12 -0300

    Modificando para realizar o desafio

--- ModuloDeClassesEMetodos/classe/Produto.java
+++ ModuloDeClassesEMetodos/classe/Produto.java
@@ -7,12 +7,47 @@ public class Produto {
 	
 	Produto() {}
 	
-	Produto(String nome, double preco) {
+	public Produto(String nome, double preco, double desconto) {
 		this.nome = nome;
 		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public String getNome() {
+		return nome;
 	}
 	
 	double precoComDesconto() {
 		return this.preco * (1 - this.desconto);
 	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public static double getDesconto() {
+		return desconto;
+	}
+
+	public static void setDesconto(double desconto) {
+		Produto.desconto = desconto;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public static String teste(String t) {
+		return t;
+	}
+	
 }
